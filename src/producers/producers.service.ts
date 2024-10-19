d28946eb84b90142b696d472c135aaf5326a73f5
André Lobo <loboandre@hotmail.com>
2024-10-19 08:44:13 -0400

    feat: add Dockerfile and docker-compose

--- src/producers/producers.service.ts
+++ src/producers/producers.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
-import { Producer } from './entities/producers.entity';
+import { Producer } from './entities/producer.entity';
 import { CreateProducerDto } from './dto/create-producer.dto';
 
 @Injectable()
