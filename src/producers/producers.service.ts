ec0504954f50899eb28a633e86916bb274f0fced
André Lobo <loboandre@hotmail.com>
2024-10-19 08:57:44 -0400

    fix: add validation with cpf-cnpj-validator

--- src/producers/producers.service.ts
+++ src/producers/producers.service.ts
@@ -1,6 +1,7 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, BadRequestException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import * as cpfCnpj from 'cpf-cnpj-validator';
 import { Producer } from './entities/producer.entity';
 import { CreateProducerDto } from './dto/create-producer.dto';
 
@@ -11,17 +12,49 @@ export class ProducersService {
     private producersRepository: Repository<Producer>,
   ) {}
 
-  create(createProducerDto: CreateProducerDto): Promise<Producer> {
+  async create(createProducerDto: CreateProducerDto): Promise<Producer> {
+    // Validação de CPF/CNPJ
+    const isCpfValid = cpfCnpj.cpf.isValid(createProducerDto.cpfOrCnpj);
+    const isCnpjValid = cpfCnpj.cnpj.isValid(createProducerDto.cpfOrCnpj);
+    if (!isCpfValid && !isCnpjValid) {
+      throw new BadRequestException('CPF ou CNPJ inválido.');
+    }
+
+    // Validação de áreas
+    if (
+      createProducerDto.arableArea + createProducerDto.vegetationArea >
+      createProducerDto.totalArea
+    ) {
+      throw new BadRequestException(
+        'A soma das áreas agricultável e vegetação não pode ser maior que a área total.',
+      );
+    }
+
+    // Verificar duplicatas
+    const existingProducer = await this.producersRepository.findOne({
+      where: {
+        cpfOrCnpj: createProducerDto.cpfOrCnpj,
+        farmName: createProducerDto.farmName,
+      },
+    });
+    if (existingProducer) {
+      throw new BadRequestException(
+        'Produtor já cadastrado com este CPF/CNPJ e nome de fazenda.',
+      );
+    }
+
     const producer = this.producersRepository.create(createProducerDto);
     return this.producersRepository.save(producer);
   }
+
+  // Atualizar similar para as validações
 
   findAll(): Promise<Producer[]> {
     return this.producersRepository.find();
   }
 
-  async findOne(id: number): Promise<Producer> {
-    return this.producersRepository.findOneBy({ id });
+  findOne(id: number): Promise<Producer> {
+    return this.producersRepository.findOne({ where: { id } });
   }
 
   async remove(id: number): Promise<void> {
